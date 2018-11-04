6619695b009bdfb4dc60b84028d706562013d1e0
matheusrcruz <git.dev@outlook.com>
2018-11-03 21:39:12 -0300

    ADD:New File

---
+++ script.sh
@@ -0,0 +1,1 @@
+sudo apt-get purge mysql-server mysql-client mysql-common mysql-server-core-* mysql-client-core-* && sudo apt-get remove mysql-server mysql-client mysql-common mysql-server-core-* mysql-client-core-* && sudo rm -rf /etc/mysql /var/lib/mysql && sudo apt-get autoremove && sudo apt-get autoclean
